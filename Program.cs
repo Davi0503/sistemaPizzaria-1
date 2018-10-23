e316d62b189caae2dca28937d11b1b19d0361ed5
Carlos Eduardo de Barros Souza <40256701865@info.local>
2018-10-23 09:11:41 -0300

    Criacao Projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Senai.Projetos.Sistema.Pizzaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
