e6e0e5c6e656686ce02b207747adf6586feaa974
gcmartins11 <gabriel.cmartins11@gmail.com>
2018-10-23 09:59:44 -0300

    Menu inicial

--- Program.cs
+++ Program.cs
@@ -6,7 +6,49 @@ namespace Senai.Projetos.Sistema.Pizzaria
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int menu;
+
+            do{
+                Console.WriteLine("Selecione opção desejada");
+                Console.WriteLine("[1] - Cadastrar usuário");
+                Console.WriteLine("[2] - Efetuar login");
+                Console.WriteLine("[3] - Listar usuários");
+                Console.WriteLine("[9] - Sair");
+                menu = int.Parse(Console.ReadLine());
+
+                switch (menu){
+                    
+                    //Cadastrar usuario
+                    case 1:{
+                        break;
+                    }
+
+                    //Efetuar login
+                    case 2:{
+                        break;
+                    }
+
+                    //Listar usuários
+                    case 3:{
+                        break;
+                    }
+
+
+                    case 9:{
+                        Console.WriteLine("Obrigado e volte sempre!");
+                        break;
+                    }
+
+                    //Opções inválidas                    
+                    default:{
+                        Console.WriteLine("Opção inválida");
+                    break;
+                    }
+
+                }
+
+            } while (menu != 9);
+
         }
     }
 }
