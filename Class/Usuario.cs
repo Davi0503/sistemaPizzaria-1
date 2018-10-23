73b18d385f02738bae552ebc53ee552c2cc8108b
Carlos Eduardo de Barros Souza <40256701865@info.local>
2018-10-23 10:41:31 -0300

    Criação do Metodo para verificar se email possui @

--- Class/Usuario.cs
+++ Class/Usuario.cs
@@ -4,9 +4,32 @@ namespace sistemaPizzaria.Class
 {
     public class Usuario
     {
+        #region Propriedade
         public int ID { get; set; }
         public string Nome { get; set; }
         public string Email { get; set; }
         public DateTime DataCriacao { get; set; }
+        #endregion
+
+
+        #region Métodos
+
+        /// <summary>
+        /// Método para verificar se o email informado possuí o caracter @
+        /// </summary>
+        /// //Email informado pelo usuário
+        /// <param name="email"></param>
+        public void ValidarEmail(string email)
+        {
+            if(email.Contains("@"))
+            {
+                Console.WriteLine("Email válido!");
+            }
+            else
+            {
+                Console.WriteLine("Digite um email válido");
+            }
+        } 
+        #endregion
     }
 }
