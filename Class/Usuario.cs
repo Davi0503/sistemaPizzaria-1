a7273698a40f9606e712d3cce51d00a8fcf6175a
gcmartins11 <gabriel.cmartins11@gmail.com>
2018-10-23 09:17:07 -0300

    Classe Usuario

---
+++ Class/Usuario.cs
@@ -0,0 +1,12 @@
+using System;
+namespace sistemaPizzaria.Class
+
+{
+    public class Usuario
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataCriacao { get; set; }
+    }
+}
